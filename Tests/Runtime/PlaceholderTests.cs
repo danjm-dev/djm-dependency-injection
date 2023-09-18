5a5de74c607766dca71e1dfa6aa41c31dacfad7c
Daniel Miller <danjm.dev@gmail.com>
2023-09-18 15:07:26 +1200

    Added placeholder test class to avoid warning

---
+++ Tests/Runtime/PlaceholderTests.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+
+namespace DJM.DependencyInjection.Tests
+{
+    [TestFixture]
+    internal sealed class PlaceholderTests
+    {
+        [Test]
+        public void PlaceholderTest() => Assert.IsTrue(true);
+    }
+}
